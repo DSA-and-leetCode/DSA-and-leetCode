5868eae94e6244c7fefeaf6d57087cfffed4a67b
Arhaan Sayeed <dev.sayarhaan@gmail.com>
2023-10-27 03:45:13 +0000

    first commit of the course

---
+++ functions.js
@@ -0,0 +1,30 @@
+// function saymyname(){
+//     console.log("A");
+//     console.log("R");
+//     console.log("H");
+//     console.log("A");
+//     console.log("A");
+//     console.log("N");
+// }
+
+// // saymyname();
+
+// function addition(num1, num2){
+//     return num1 + num2;
+// }
+
+// let result = addition(35, 34);
+// console.log("result: " + result);
+// console.log("type of result: " + typeof(result));
+
+// function loginUser(username){
+//     return `${username} user login confirmed`;
+// }
+// console.log(loginUser("Anonymous"));
+
+let a = 300;
+if(true){
+    let a = 30
+    console.log("Inner: ", a);
+}
+console.log(a);
