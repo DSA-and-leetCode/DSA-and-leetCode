6a084c684a0b8d4571dc32a5744b7ea85318a4fe
Arhaan Sayeed <dev.sayarhaan@gmail.com>
2023-10-27 04:51:03 +0000

    second commit

---
+++ this.js
@@ -0,0 +1,14 @@
+// function arhaan(){
+//     let usernames = arhaan;
+//     console.log(this);
+// }
+// arhaan();
+
+// const arhaan = () =>{
+//     let usernames = arhaan;
+//     console.log(this);
+// }
+// arhaan()
+
+const add = () => ({name: 'arhaan'});
+console.log(add());
